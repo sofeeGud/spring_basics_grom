91fa11ab1752d6cd14f13958624ef7ecd59fdad5
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-04-28 20:23:13 +0300

    spring les2 hw2

--- src/main/java/com/lesson2/hw2/ItemController.java
+++ src/main/java/com/lesson2/hw2/ItemController.java
@@ -1,25 +1,59 @@
 package com.lesson2.hw2;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class ItemController {
     @Autowired
     ItemService itemService;
 
 
-    public Item save(Item item) throws Exception {
-        return itemService.save(item);
+    @RequestMapping(method = RequestMethod.GET, value = "/findById/{id}", produces = "text/plain")
+    public @ResponseBody
+    String findById(@PathVariable String  id) {
+        try {
+            return itemService.findById(Long.parseLong(id)).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
-    public Item update(Item item) throws Exception{
-        return itemService.update(item);
+    @RequestMapping(method = RequestMethod.POST, value = "/save", produces = "text/json")
+    public @ResponseBody
+    String save(@RequestBody String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Item item = mapper.readValue(json, Item.class);
+          return itemService.save(item).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
-    public Item delete(Long id) throws Exception{
-        return itemService.delete(id);
+    @RequestMapping(method = RequestMethod.PUT, value = "/update", produces = "text/plain")
+    public @ResponseBody
+    String update(@RequestBody String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Item item = mapper.readValue(json, Item.class);
+            return  itemService.update(item).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
-    public Item findById(Long id) throws Exception{
-        return itemService.findById(id);
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete", produces = "text/plain")
+    public @ResponseBody
+    String delete(@RequestBody String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Item item = mapper.readValue(json, Item.class);
+            return  itemService.delete(item.getId()).toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 }
