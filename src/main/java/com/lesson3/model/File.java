9831632bd683b1def275b5c28e9bd9ebbe86fb5a
Lenovo T540p <sonychkalapochka94@gmail.com>
2020-05-23 15:44:59 +0300

    spring les3

--- src/main/java/com/lesson3/model/File.java
+++ src/main/java/com/lesson3/model/File.java
@@ -1,20 +1,34 @@
 package com.lesson3.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
-
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @Entity
 @Table(name = "FILES")
 public class File {
     @JsonProperty("id")
+    @Id
+    @SequenceGenerator(name = "FILE_ID_SEQ", sequenceName = "FILE_ID_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FILE_ID_SEQ")
+    @Column(name = "ID")
     private long id;
     @JsonProperty("name")
+    @Column(name = "NAME")
     private String name;
     @JsonProperty("format")
+    @Column(name = "FORMAT")
     private String format;
     @JsonProperty("size")
+    @Column(name = "FILE_SIZE")
     private long size;
 
     @JsonProperty("storage")
@@ -22,87 +36,4 @@ public class File {
     @JoinColumn(name="STORAGE_ID", nullable = false)
     Storage storage;
 
-    @Id
-    @SequenceGenerator(name = "FILE_ID_SEQ", sequenceName = "FILE_ID_SEQ", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FILE_ID_SEQ")
-    @Column(name = "ID")
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @Column(name = "NAME")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Column(name = "FORMAT")
-    public String getFormat() {
-        return format;
-    }
-
-    public void setFormat(String format) {
-        this.format = format;
-    }
-
-    @Column(name = "FILE_SIZE")
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
-
-    @ManyToOne
-    @JoinColumn(name="STORAGE_ID", nullable = false)
-    public Storage getStorage() {
-        return storage;
-    }
-
-    public void setStorage(Storage storage) {
-        this.storage = storage;
-    }
-
-    @Override
-    public String toString() {
-        return "File{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", format='" + format + '\'' +
-                ", size=" + size +
-                ", storage=" + storage +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        File file = (File) o;
-
-        if (id != file.id) return false;
-        if (size != file.size) return false;
-        if (!name.equals(file.name)) return false;
-        if (!format.equals(file.format)) return false;
-        return storage.equals(file.storage);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + name.hashCode();
-        result = 31 * result + format.hashCode();
-        result = 31 * result + (int) (size ^ (size >>> 32));
-        result = 31 * result + storage.hashCode();
-        return result;
-    }
 }
--- src/main/java/com/lesson3/model/Storage.java
+++ src/main/java/com/lesson3/model/Storage.java
@@ -1,89 +1,34 @@
 package com.lesson3.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @Entity
 @Table(name = "STORAGE")
 public class Storage {
     @JsonProperty("id")
-    private long id;
-    @JsonProperty("formatsSupported")
-    private String formatsSupported;
-    @JsonProperty("storageCountry")
-    private String storageCountry;
-    @JsonProperty("storageSize")
-    private long storageSize;
-
     @Id
     @SequenceGenerator(name = "STORAGE_ID_SEQ", sequenceName = "STORAGE_ID_SEQ", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "STORAGE_ID_SEQ")
     @Column(name = "ID")
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
+    private long id;
+    @JsonProperty("formatsSupported")
     @Column(name = "FORMATS_SUPPORTED")
-    public String getFormatsSupported() {
-        return formatsSupported;
-    }
-
-    public void setFormatsSupported(String formatsSupported) {
-        this.formatsSupported = formatsSupported;
-    }
-
+    private String formatsSupported;
+    @JsonProperty("storageCountry")
     @Column(name = "STORAGE_COUNTRY")
-    public String getStorageCountry() {
-        return storageCountry;
-    }
-
-    public void setStorageCountry(String storageCountry) {
-        this.storageCountry = storageCountry;
-    }
-
+    private String storageCountry;
+    @JsonProperty("storageSize")
     @Column(name = "STORAGE_SIZE")
-    public long getStorageSize() {
-        return storageSize;
-    }
+    private long storageSize;
 
-    public void setStorageSize(long storageSize) {
-        this.storageSize = storageSize;
-    }
-
-    @Override
-    public String toString() {
-        return "Storage{" +
-                "id=" + id +
-                ", formatsSupported=" + formatsSupported +
-                ", storageCountry='" + storageCountry + '\'' +
-                ", storageSize=" + storageSize +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Storage storage = (Storage) o;
-
-        if (id != storage.id) return false;
-        if (storageSize != storage.storageSize) return false;
-        if (!formatsSupported.equals(storage.formatsSupported)) return false;
-        return storageCountry.equals(storage.storageCountry);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + formatsSupported.hashCode();
-        result = 31 * result + storageCountry.hashCode();
-        result = 31 * result + (int) (storageSize ^ (storageSize >>> 32));
-        return result;
-    }
 }
